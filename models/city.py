e6215371a8db642e2a151202e3109e599b8e8340
Youlman <youlmankamal@gmail.com>
2023-03-22 18:33:44 +0000

    update Place, city, User  using Sqlalchemy

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """Defines the City class."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -16,3 +16,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
