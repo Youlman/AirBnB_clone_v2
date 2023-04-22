8d8fdf20c717a1c8cc2fa776e79580d1335d46cc
Youlman <youlmankamal@gmail.com>
2023-04-22 01:46:07 +0000

    hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,5 +15,11 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB!'"""
+    return "HBNB"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
