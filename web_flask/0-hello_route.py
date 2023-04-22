34cf9070156ffae09ea9b62b97a73524673b44b4
Youlman <youlmankamal@gmail.com>
2023-04-22 00:26:05 +0000

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,10 @@ Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
 """
-
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
